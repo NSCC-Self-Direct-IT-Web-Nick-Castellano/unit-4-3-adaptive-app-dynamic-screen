ad0ad71d51a92726f7648303c54f09b37f0fd202
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-26 19:58:15 -0400

    completed excercise

---
+++ app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,6 @@
+package com.example.reply.ui.utils
+
+// add enum classes for handling adaptive navigation type
+enum class ReplyNavigationType {
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
